e51213cdcf7aa1ecdd04381e6d26175dbb28b0bb
Eitan Sela <eitan.sela@gmail.com>
2022-06-20 16:38:47 +0300

    Initial push for tensorflow_deploy_model_using_boto3

---
+++ tensorflow_deploy_model_using_boto3/code/inference.py
@@ -0,0 +1,52 @@
+import json
+import os
+import requests
+
+
+def handler(data, context):
+    """Handle request.
+    Args:
+        data (obj): the request data
+        context (Context): an object containing request and configuration details
+    Returns:
+        (bytes, string): data to return to client, (optional) response content type
+    """
+
+    print("handler start")
+    
+    my_env_var_1 = os.environ.get('MY_ENV_VAR_1')
+    my_env_var_2 = os.environ.get('MY_ENV_VAR_2')
+    print(f"ENV Variable MY_ENV_VAR_1 value: {my_env_var_1}")
+    print(f"ENV Variable MY_ENV_VAR_2 value: {my_env_var_2}")
+
+    processed_input = _process_input(data, context)
+    response = requests.post(context.rest_uri, data=processed_input)
+    return _process_output(response, context)
+
+
+def _process_input(data, context):
+    if context.request_content_type == 'application/json':
+        # pass through json (assumes it's correctly formed)
+        d = data.read().decode('utf-8')
+        print('input data: {}'.format(d))
+        input_json = json.loads(d)
+
+        return input_json
+
+    if context.request_content_type == 'text/csv':
+        # very simple csv handler
+        return json.dumps({
+            'instances': [float(x) for x in data.read().decode('utf-8').split(',')]
+        })
+
+    raise ValueError('{{"error": "unsupported content type {}"}}'.format(
+        context.request_content_type or "unknown"))
+
+
+def _process_output(data, context):
+    if data.status_code != 200:
+        raise ValueError(data.content.decode('utf-8'))
+
+    response_content_type = context.accept_header
+    prediction = data.content
+    return prediction, response_content_type
