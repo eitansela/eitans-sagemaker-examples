c74098476201a0662b804877cd67cd847b25897b
Eitan Sela <eitan.sela@gmail.com>
2023-03-05 20:18:59 +0200

    Initial push for sagemaker_pytorch_binary_segmentation_intro

---
+++ sagemaker_pytorch_binary_segmentation_intro/code/train.py
@@ -0,0 +1,217 @@
+# ensure that the latest version of the SageMaker SDK is available
+import os
+
+import argparse
+import logging
+import json
+import glob
+import boto3
+
+import torch
+import pytorch_lightning as pl
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch.datasets import SimpleOxfordPetDataset
+from torch.utils.data import DataLoader
+
+
+class PetModel(pl.LightningModule):
+
+    def __init__(self, arch, encoder_name, in_channels, out_classes, lr, **kwargs):
+        super().__init__()
+        self.model = smp.create_model(
+            arch, encoder_name=encoder_name, in_channels=in_channels, classes=out_classes, **kwargs
+        )
+        self.lr = lr
+
+        # preprocessing parameteres for image
+        params = smp.encoders.get_preprocessing_params(encoder_name)
+        self.register_buffer("std", torch.tensor(params["std"]).view(1, 3, 1, 1))
+        self.register_buffer("mean", torch.tensor(params["mean"]).view(1, 3, 1, 1))
+
+        # for image segmentation dice loss could be the best first choice
+        self.loss_fn = smp.losses.DiceLoss(smp.losses.BINARY_MODE, from_logits=True)
+
+    def forward(self, image):
+        # normalize image here
+        image = (image - self.mean) / self.std
+        mask = self.model(image)
+        return mask
+
+    def shared_step(self, batch, stage):
+        
+        image = batch["image"]
+
+        # Shape of the image should be (batch_size, num_channels, height, width)
+        # if you work with grayscale images, expand channels dim to have [batch_size, 1, height, width]
+        assert image.ndim == 4
+
+        # Check that image dimensions are divisible by 32, 
+        # encoder and decoder connected by `skip connections` and usually encoder have 5 stages of 
+        # downsampling by factor 2 (2 ^ 5 = 32); e.g. if we have image with shape 65x65 we will have 
+        # following shapes of features in encoder and decoder: 84, 42, 21, 10, 5 -> 5, 10, 20, 40, 80
+        # and we will get an error trying to concat these features
+        h, w = image.shape[2:]
+        assert h % 32 == 0 and w % 32 == 0
+
+        mask = batch["mask"]
+
+        # Shape of the mask should be [batch_size, num_classes, height, width]
+        # for binary segmentation num_classes = 1
+        assert mask.ndim == 4
+
+        # Check that mask values in between 0 and 1, NOT 0 and 255 for binary segmentation
+        assert mask.max() <= 1.0 and mask.min() >= 0
+
+        logits_mask = self.forward(image)
+        
+        # Predicted mask contains logits, and loss_fn param `from_logits` is set to True
+        loss = self.loss_fn(logits_mask, mask)
+
+        # Lets compute metrics for some threshold
+        # first convert mask values to probabilities, then 
+        # apply thresholding
+        prob_mask = logits_mask.sigmoid()
+        pred_mask = (prob_mask > 0.5).float()
+
+        # We will compute IoU metric by two ways
+        #   1. dataset-wise
+        #   2. image-wise
+        # but for now we just compute true positive, false positive, false negative and
+        # true negative 'pixels' for each image and class
+        # these values will be aggregated in the end of an epoch
+        tp, fp, fn, tn = smp.metrics.get_stats(pred_mask.long(), mask.long(), mode="binary")
+
+        return {
+            "loss": loss,
+            "tp": tp,
+            "fp": fp,
+            "fn": fn,
+            "tn": tn,
+        }
+
+    def shared_epoch_end(self, outputs, stage):
+        # aggregate step metics
+        tp = torch.cat([x["tp"] for x in outputs])
+        fp = torch.cat([x["fp"] for x in outputs])
+        fn = torch.cat([x["fn"] for x in outputs])
+        tn = torch.cat([x["tn"] for x in outputs])
+
+        # per image IoU means that we first calculate IoU score for each image 
+        # and then compute mean over these scores
+        per_image_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro-imagewise")
+        
+        # dataset IoU means that we aggregate intersection and union over whole dataset
+        # and then compute IoU score. The difference between dataset_iou and per_image_iou scores
+        # in this particular case will not be much, however for dataset 
+        # with "empty" images (images without target class) a large gap could be observed. 
+        # Empty images influence a lot on per_image_iou and much less on dataset_iou.
+        dataset_iou = smp.metrics.iou_score(tp, fp, fn, tn, reduction="micro")
+
+        metrics = {
+            f"{stage}_per_image_iou": per_image_iou,
+            f"{stage}_dataset_iou": dataset_iou,
+        }
+        
+        self.log_dict(metrics, prog_bar=True)
+
+    def training_step(self, batch, batch_idx):
+        return self.shared_step(batch, "train")            
+
+    def training_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch, "valid")
+
+    def validation_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "valid")
+
+    def test_step(self, batch, batch_idx):
+        return self.shared_step(batch, "test")  
+
+    def test_epoch_end(self, outputs):
+        return self.shared_epoch_end(outputs, "test")
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
+
+
+def _train(args):
+    print("_train - Start")
+    
+    data_dir_path = args.data_dir + "/*"
+    print(f"Training data: {glob.glob(data_dir_path)}")
+    
+    # init train, val, test sets
+    train_dataset = SimpleOxfordPetDataset(args.data_dir, "train")
+    valid_dataset = SimpleOxfordPetDataset(args.data_dir, "valid")
+    test_dataset = SimpleOxfordPetDataset(args.data_dir, "test")
+    
+    # It is a good practice to check datasets don`t intersects with each other
+    assert set(test_dataset.filenames).isdisjoint(set(train_dataset.filenames))
+    assert set(test_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+    assert set(train_dataset.filenames).isdisjoint(set(valid_dataset.filenames))
+    
+    print(f"Train size: {len(train_dataset)}")
+    print(f"Valid size: {len(valid_dataset)}")
+    print(f"Test size: {len(test_dataset)}")
+
+    n_cpu = os.cpu_count()
+    train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=n_cpu)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+    test_dataloader = DataLoader(test_dataset, batch_size=16, shuffle=False, num_workers=n_cpu)
+
+    model = PetModel(args.arch, "resnet34", in_channels=3, out_classes=1, lr=args.lr)
+
+    trainer = pl.Trainer(
+        accelerator='gpu', 
+        devices=1,
+        max_epochs=args.epochs,
+    )
+
+    trainer.fit(
+        model, 
+        train_dataloaders=train_dataloader, 
+        val_dataloaders=valid_dataloader,
+    )
+
+    # run validation dataset
+    valid_metrics = trainer.validate(model, dataloaders=valid_dataloader, verbose=False)
+    print(f"valid_dataset_iou: {valid_metrics[0]['valid_dataset_iou']}")
+    print(f"valid_per_image_iou: {valid_metrics[0]['valid_per_image_iou']}")
+    
+    # run test dataset
+    test_metrics = trainer.test(model, dataloaders=test_dataloader, verbose=False)
+    print(f"test_dataset_iou: {test_metrics[0]['test_dataset_iou']}")
+    print(f"test_per_image_iou: {test_metrics[0]['test_per_image_iou']}")
+
+    model_output_path = os.path.join(args.model_dir, 'model.pth')
+    print(f"Saving model in: {model_output_path}")
+    torch.save(model.state_dict(), model_output_path)
+
+    print("_train - End")
+
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--workers', type=int, default=2, metavar='W',
+                        help='number of data loading workers (default: 2)')
+    parser.add_argument('--epochs', type=int, default=2, metavar='E',
+                        help='number of total epochs to run (default: 2)')
+    parser.add_argument('--batch_size', type=int, default=4, metavar='BS',
+                        help='batch size (default: 4)')
+    parser.add_argument('--lr', type=float, default=0.001, metavar='LR',
+                        help='initial learning rate (default: 0.001)')
+    parser.add_argument('--arch', type=str, default="FPN")
+    parser.add_argument('--momentum', type=float, default=0.9, metavar='M', help='momentum (default: 0.9)')
+    parser.add_argument('--dist_backend', type=str, default='gloo', help='distributed backend (default: gloo)')
+    parser.add_argument("--region", type=str, default="us-east-1", help="SageMaker Region")
+
+    parser.add_argument('--hosts', type=list, default=json.loads(os.environ['SM_HOSTS']))
+    parser.add_argument('--current-host', type=str, default=os.environ['SM_CURRENT_HOST'])
+    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--num-gpus', type=int, default=os.environ['SM_NUM_GPUS'])
+    
+    _train(parser.parse_args())
